5ab6509ada65489a09733f203dde4b211c6255ed
carbonyle elynobrac <carbonyle@users.noreply.github.com>
2015-08-04 18:06:36 +0200

    Update gpg_wizard.sh

--- gpg_wizard.sh
+++ gpg_wizard.sh
@@ -14,26 +14,40 @@ export PINENTRY_USER_DATA="USE_CURSES=1"
 gpg-agent --use-standard-socket --daemon
 clear
 else
-echo "Insert Yubikey & run again" && exit 1
+echo "Insert Yubikey & run again" && osascript -e 'display notification "You might have forgotten something" with title "AI"'&& exit 1
 fi
 
 #menu
 
+osascript -e 'display notification "Greetings" with title "AI"'
+
 read -p "$(echo -e '
-1: Asymmetric (with signature)\b
-2: Asymmetric (without key_id)\b
-3: Symmetric \b
-4: Create encrypted image (not using GPG)\b
-5: Sign \b
-6: Verify \b
-7: Enter smartcard menu \b
+1: Asymmetric \b
+2: Symmetric \b
+3: Decrypt \b
+4: Sign \b
+5: Verify \b
+6: Smartcard menu \b
+7: Other \b
 8: exit\n\b')" choice
 
-if [[ $choice =~ ^([1])$ ]]
-    then read -p "$(echo -e 'File to encrypt: \n\b')" file_to_encrypt && gpg -r $USER -e $file_to_encrypt && gpg -sb $file_to_encrypt.gpg
+    if [[ $choice =~ ^([1])$ ]]
+    then read -p "$(echo -e '
+1: To self, with signature\b
+2: To self, without key_id\b
+3: To:
+4: exit\n\b')" asymm_option
+        if [[ $asymm_option =~ ^([1])$ ]]
+            then read -p "$(echo -e 'File to encrypt: \n\b')" file_to_encrypt && gpg -r $USER -e $file_to_encrypt && gpg -sb $file_to_encrypt.gpg
+            elif [[ $asymm_option =~ ^([2])$ ]]
+            then read -p "$(echo -e 'File to encrypt: \n\b')" file_to_encrypt && gpg -r $USER --hidden-recipient $USER --throw-keyids -e $file_to_encrypt
+            elif [[ $asymm_option =~ ^([3])$ ]]
+            then read -p "$(echo -e 'Email of the recipient: \n\b')" recipient && read -p "$(echo -e 'File to encrypt: \n\b')" file_to_encrypt && gpg -r $recipient -e $file_to_encrypt
+            elif [[ $asymm_option =~ ^([4])$ ]]
+            then  echo "bye" && exit 0
+            else echo "Invalid choice" && osascript -e 'display notification "What happened?" with title "AI"' && exit 1
+        fi
     elif [[ $choice =~ ^([2])$ ]]
-    then read -p "$(echo -e 'File to encrypt: \n\b')" file_to_encrypt && gpg -r $USER --hidden-recipient $USER --throw-keyids -e $file_to_encrypt
-    elif [[ $choice =~ ^([3])$ ]]
     then read -p "$(echo -e 'File to encrypt: \n\b')" file_to_encrypt && read -p "$(echo -e 'Cipher: [A]ES, [B]lowfish or [T]wofish \n\b')" Symm_choice
         if [[ $Symm_choice =~ ^([aA])$ ]]
             then gpg --symmetric --cipher-algo AES256 $file_to_encrypt
@@ -41,38 +55,57 @@ if [[ $choice =~ ^([1])$ ]]
             then gpg --symmetric --cipher-algo BLOWFISH $file_to_encrypt
             elif [[ $Symm_choice =~ ^([tT])$ ]]
             then gpg --symmetric --cipher-algo TWOFISH $file_to_encrypt
-            else echo "Invalid choice" && exit 1
+            else echo "Invalid choice" && osascript -e 'display notification "What happened?" with title "AI"' && exit 1
         fi
+    elif [[ $choice =~ ^([3])$ ]]
+    then read -p "$(echo -e 'File to decrypt: \n\b')" file_to_decrypt && gpg -o /Users/$USER/Desktop/"$(basename $file_to_decrypt .gpg)" -d $file_to_decrypt
     elif [[ $choice =~ ^([4])$ ]]
-    then read -p "$(echo -e 'size in MB (50m)\n\b')" size && read -p "$(echo -e 'name \n\b')" name && read -p "$(echo -e 'path \n\b')" path  && hdiutil create -size $size -fs HFS+ -encryption AES-256 -volname $name $path/$name.dmg
-    elif [[ $choice =~ ^([5])$ ]]
     then read -p "$(echo -e 'File to sign: \n\b')" file_to_sign && gpg -sb $file_to_sign
-    elif [[ $choice =~ ^([6])$ ]]
+    elif [[ $choice =~ ^([5])$ ]]
     then read -p "$(echo -e 'File to verify: \n\b')" file_to_verify && gpg --verify $file_to_verify
-    elif [[ $choice =~ ^([7])$ ]]
+    elif [[ $choice =~ ^([6])$ ]]
     then clear && read -p "$(echo -e '
 1: Card status\b
 2: Show public keys\b
 3: Show private keys\b
-4: Edit card\b
-5: exit\n\b')" card_operation
+4: Import public key
+5: Edit card\b
+6: exit\n\b')" card_operation
             if [[ $card_operation =~ ^([1])$ ]]
                 then gpg --card-status
                 elif [[ $card_operation =~ ^([2])$ ]]
-                then gpg --list-keys
+                then gpg --list-keys | grep @
                 elif [[ $card_operation =~ ^([3])$ ]]
-                then gpg --list-private-keys
+                then gpg --list-secret-keys
                 elif [[ $card_operation =~ ^([4])$ ]]
+                then read -p "$(echo -e 'Key to import\n\b')" import && gpg --import $import
+                elif [[ $card_operation =~ ^([5])$ ]]
                 then read -p "$(echo -e 'Are you sure [Y]? \n\b')" confirmation
                             if [[ $confirmation =~ ^([Y])$ ]]
                                 then gpg --card-edit
-                                else exit 1
+                                else osascript -e 'display notification "You shall not pass!" with title "AI"' && exit 1
                             fi
-                elif [[ $card_operation =~ ^([5])$ ]]
-                then echo "bye" && exit 0
-                else echo "invalid choice" && exit 1
+                elif [[ $card_operation =~ ^([6])$ ]]
+                then echo "bye" && osascript -e 'display notification "See you again!" with title "AI"' && exit 0
+                else echo "invalid choice" && osascript -e 'display notification "What happened?" with title "AI"' && exit 1
+            fi
+    elif [[ $choice =~ ^([7])$ ]]
+    then clear && read -p "$(echo -e '
+1: Create a encrypted container (not using GPG)\b
+2: \b
+3: \b
+4: exit\n\b')" other_options
+            if [[ $other_options =~ ^([1])$ ]]
+                then read -p "$(echo -e 'size in MB (50m)\n\b')" size && read -p "$(echo -e 'name \n\b')" name && read -p "$(echo -e 'path \n\b')" path  && hdiutil create -size $size -fs HFS+ -encryption AES-256 -volname $name $path/$name.dmg
+                elif [[ $other_options =~ ^([2])$ ]]
+                then exit 0
+                elif [[ $other_options =~ ^([3])$ ]]
+                then exit 0
+                elif [[ $other_options =~ ^([4])$ ]]
+                then echo "bye" && osascript -e 'display notification "See you again!" with title "AI"' && exit 0
+                else echo "invalid choice" && osascript -e 'display notification "What happened?" with title "AI"' && exit 1
             fi
     elif [[ $choice =~ ^([8])$ ]]
-    then echo "bye" && exit 0
-    else echo "invalid choice" && exit 1
+    then echo "bye" && osascript -e 'display notification "See you again!" with title "AI"' && exit 0
+    else echo "invalid choice" && osascript -e 'display notification "What happened?" with title "AI"' && exit 1
 fi
